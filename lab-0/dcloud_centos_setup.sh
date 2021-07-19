ccba3d0923f7500ca0c09a94189840a1ca5b3e60
johncui0505 <86839433+johncui0505@users.noreply.github.com>
2021-07-19 09:05:40 +0800

    Update dcloud_centos_setup.sh

--- lab-0/dcloud_centos_setup.sh
+++ lab-0/dcloud_centos_setup.sh
@@ -5,12 +5,13 @@ VERSION_ANSIBLE=2.9.13
 VERSION_ANSIBLE_ACI=2.0.0 
 
 echo "================= Installing Ansible -->" \
-    && yes | pip uninstall ansible
+    && yes | pip uninstall ansible \
     && pip install ansible==${VERSION_ANSIBLE} \
     && ansible --version
 
 echo "================= Installing Ansible Cisco ACI Collection: ${VERSION_ANSIBLE_ACI} -->" \
-    && ansible-galaxy collection install cisco.aci:==${VERSION_ANSIBLE_ACI} 
+    && ansible-galaxy collection install cisco.aci:==${VERSION_ANSIBLE_ACI} \
+    && ansible-galaxy collection install community.general 
 
 echo "================= Installing Terraform ${VERSION_TERRAFORM} -->"  \
     && curl -sSL -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${VERSION_TERRAFORM}/terraform_${VERSION_TERRAFORM}_linux_amd64.zip 2>&1 \
